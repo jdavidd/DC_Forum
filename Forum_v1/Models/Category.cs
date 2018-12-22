467f19d2f08e4db4d56e10fddba4e11633cb115a
david <j.davidd@yahoo.com>
2018-12-22 18:19:05 +0200

    first commit

---
+++ Forum_v1/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Forum_v1.Models
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string Title { get; set; }
+
+        public string UserId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+
+    }
+
+}
